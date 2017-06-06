a534a78777a1c8be0c3e53dfcf25dfdc0c6de716
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-06-06 14:04:25 -0400

    More Oop

--- map/my_map/sketch.js
+++ map/my_map/sketch.js
@@ -1,6 +1,8 @@
 function Thing(location){
   this.id = location.id;
   this.shape = null;
+  this.music = null;
+  this.color = null;
   this.x = location.x;
   this.y = location.y;
   this.get_location = function (){
@@ -9,24 +11,44 @@ function Thing(location){
     tmp.push(this.y);
     return tmp
   }
+  this.set_location = function (x,y){
+    this.x = x;
+    this.y = y;
+  }
+  this.update_color = function(color){
+    this.color = color;
+    this.shape.fill(this.color)
+  }
   return this;
 }
 
 function Person(location){
   Thing.call(this, location)
-
-  this.shape = r.ellipse(this.x, this.y, 30, 30);
   this.move = function(dx, dy){
     this.x = this.x + dx;
     this.y = this.y + dy;
     this.shape.move(this.x, this.y, 0, true)
   }
+
+  this.width = 50;
+  this.height = 50;
+  this.shape = new Rune.Ellipse(this.x, this.y,
+    this.width, this.height)
+    .fill(this.color).addTo(myGroup);
+
+  this.update_size = function(width, height){
+    this.width = width;
+    this.height = height;
+    this.shape.state.width = width;
+    this.shape.state.height= height;
+  }
   return this;
+
 }
 
 function Beacon(location){
   Thing.call(this, location)
-  this.shape = r.triangle(0, 0, 100, 50);
+  this.shape = new Rune.triangle(0, 0, 100, 50, r);
   return this;
 }
 
@@ -39,6 +61,9 @@ var r = new Rune({
   debug: true
 });
 
+var myGroup = r.group(0, 0)
+
+
 my_location = {
   id: 1,
   type: "person",
@@ -47,9 +72,17 @@ my_location = {
 }
 
 me = new Person(my_location)
+me_color = new Rune.Color('hsv', 0, 50, 50, 0.3)
+me.update_color(me_color)
 console.log(me)
+
+me2 = new Person(my_location)
+me2.update_size(100, 100)
+
+
 r.on('update', function() {
-  me.move(Rune.random(-10,10), Rune.random(-10,10))
+  me.move(Rune.random(-5,5), Rune.random(-5,5))
+  me2.move(Rune.random(-5,5), Rune.random(-5,5))
 });
 
 r.play()
